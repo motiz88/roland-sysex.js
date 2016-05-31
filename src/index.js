1793b16839f1f228aace69b9c497173b30f01cc0
Moti Zilberman <motiz88@gmail.com>
2016-05-31 09:41:07 +0300

    docs: Add basic documentation using documentation.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,35 @@ export const SET_LISTENING_DEVICES = myDuck.defineType('SET_LISTENING_DEVICES');
 export const RECEIVE_MIDI_MESSAGE = myDuck.defineType('RECEIVE_MIDI_MESSAGE');
 export const SEND_MIDI_MESSAGE = myDuck.defineType('SEND_MIDI_MESSAGE');
 
+/**
+ * Creates a `RECEIVE_DEVICE_LIST` action.
+ * This action is dispatched by the MIDI enhancer at initialization, when devices are connected/disconnected and when devices change state.
+ * @param {Array<MIDIDevice>} devices
+ */
 export const receiveDeviceList = myDuck.createAction(RECEIVE_DEVICE_LIST);
+
+/**
+ * Creates a `SET_LISTENING_DEVICES` action. Dispatch this action with the `id`s of MIDI input devices you would like to receive messages from.
+ * @param {Array<string>} listeningDevices
+ */
 export const setListeningDevices = myDuck.createAction(SET_LISTENING_DEVICES);
+
+/**
+ * Creates a `RECEIVE_MIDI_MESSAGE` action. This action is dispatched by the MIDI enhancer when a message received from a device you're listening to.
+ * @param {MIDIMessage} message
+ * @param {Uint8Array} message.data
+ * @param {DOMHighResTimeStamp} message.timestamp
+ * @param {string} message.device - ID of the source device
+ */
 export const receiveMidiMessage = myDuck.createAction(RECEIVE_MIDI_MESSAGE);
+
+/**
+ * Creates a `SEND_MIDI_MESSAGE` action. Dispatch this action to send a MIDI message to a specified device.
+ * @param {MIDIMessage} message
+ * @param {Uint8Array} message.data
+ * @param {DOMHighResTimeStamp} message.timestamp
+ * @param {string} message.device - ID of the destination device
+ */
 export const sendMidiMessage = myDuck.createAction(SEND_MIDI_MESSAGE);
 
 const initialState = {
@@ -38,6 +64,12 @@ import deepEqual from 'deep-equal';
 
 const defaultRequestMIDIAccess = global && global.navigator && global.navigator.requestMIDIAccess.bind(global.navigator);
 
+/**
+ * Create a Redux store enhancer wrapping MIDI I/O and device discovery.
+ * @param {MIDIOptions} [$0.midiOptions] - Options with which to invoke `requestMIDIAccess`.
+ * @param {string} [$0.stateKey='midi'] - The key under which the enhancer will store MIDI device information in the state.
+ * @param {function(MIDIOptions): Promise<MIDIAccess>} [$0.requestMIDIAccess=navigator.requestMIDIAccess] - Web MIDI API entry point.
+ */
 export const makeMidiEnhancer = ({midiOptions, stateKey = 'midi', requestMIDIAccess = defaultRequestMIDIAccess}) => next => (userReducer, preloadedState) => {
   let midiAccess = null;
 
