56e7ddc177d15593f907222cdc55cf081c3e4c23
Moti Zilberman <motiz88@gmail.com>
2016-06-02 11:39:59 +0300

    chore(travis): Re-encrypt doc deploy key

--- travis-gen-deploy-key.sh
+++ travis-gen-deploy-key.sh
@@ -5,5 +5,5 @@ rm -f deploy_key.*
 ssh-keygen -t rsa -b 4096 -f deploy_key 
 travis encrypt-file deploy_key | \
     grep -o -P "encrypted_\K[0-9a-f]+" | \
-    head -n 1 |
+    head -n 1 \
     > travis-encryption-label
