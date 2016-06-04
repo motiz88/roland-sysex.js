9c8405bff9c913fa3d21ce432e47561100f8da94
Moti Zilberman <motiz88@gmail.com>
2016-06-04 12:18:22 +0300

    fix: Checksum calculation edge case
    Also add more test data from GR-55

--- src/rolandChecksum.js
+++ src/rolandChecksum.js
@@ -1,3 +1,3 @@
 export default function rolandChecksum (buf) {
-  return (128 - buf.reduce((a, b) => (a + b) % 128, 0));
+  return (128 - buf.reduce((a, b) => (a + b) % 128, 0)) % 128;
 }
--- test/specs/rolandChecksum.js
+++ test/specs/rolandChecksum.js
@@ -9,5 +9,7 @@ describe('rolandChecksum', () => {
   it('should be correct', () => {
     rolandChecksum(new Buffer([0x00, 0x00, 0x00, 0x04, 0x0C, 0x08]))
       .should.equal(0x68);
+    rolandChecksum(new Buffer([0x80]))
+      .should.equal(0x00);
   });
 });
